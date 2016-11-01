8d9ab9b3d57c6d07103c53459067ec6f14a54085
Martynas Žuravliovas <martynas.zuravliovas@gmail.com>
2016-11-01 21:17:52 +0200

    master: Core

---
+++ Assets/Standard Assets/2D/Scripts/MovementScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+    public float playerSpeed = 10; //speed player moves
+   
+    void Update()
+    {
+        MoveForward(); // Player Movement 
+    }
+
+    void MoveForward()
+    {
+        if (Input.GetKey("up"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("down"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("left"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("right"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
