b852c44b40b299b033cf8a5064f1502022f1e444
Martynas Žuravliovas <martynas.zuravliovas@gmail.com>
2016-11-01 22:49:41 +0200

    master: enemy movement script

--- Assets/BarakoVagis/Scripts/EnemyMovementScript.cs
+++ Assets/BarakoVagis/Scripts/EnemyMovementScript.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class EnemyMovementScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Transform targetA;
+    public Transform targetB;
+    public Transform currentTarget;
+    public float speed;
+    void Update()
+    {
+        if (currentTarget == null)
+        {
+            currentTarget = targetA;
+        }
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, step);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "Player")
+        {
+            currentTarget = currentTarget == targetA ? targetB : targetA;
+        }
+
+    }
 }
