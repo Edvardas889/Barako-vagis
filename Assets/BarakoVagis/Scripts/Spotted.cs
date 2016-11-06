63dca40864d4925e4f6e04ef1208c88a56956e79
Edvardas889 <etereikis@gmail.com>
2016-11-06 17:03:33 +0200

    GameOverOnVisionScope

---
+++ Assets/BarakoVagis/Scripts/Spotted.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spotted : MonoBehaviour {
+
+    public GameObject gameOverText;
+    public GameObject helpText;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Spotted"))
+        {
+            gameOverText.SetActive(true);
+            helpText.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
