6213723c061020aedc9dc4b1c3908989a8d6ab18
Martynas Žuravliovas <martynas.zuravliovas@gmail.com>
2016-12-11 20:45:54 +0200

    apsaugos kamera

---
+++ Assets/BarakoVagis/Scripts/SecurityCameraScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecurityCameraScript : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+    public float rotationZStart;
+    public float rotationZEnd;
+    Quaternion qStart, qEnd;
+    private float startTime;
+
+    void Start()
+    {
+        qStart = Quaternion.AngleAxis(rotationZStart, Vector3.forward);
+        qEnd = Quaternion.AngleAxis(rotationZEnd, Vector3.forward);
+    }
+
+    void Update()
+    {
+        startTime += Time.deltaTime;
+        transform.rotation = Quaternion.Lerp(qStart, qEnd, (Mathf.Sin(startTime * speed) + 1.0f) / 2.0f);
+    }
+
+}
